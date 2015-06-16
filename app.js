f1513480f5249049939307a207ed07f6fb522493
plsr <christianpoplawski@gmail.com>
2015-06-16 08:57:35 +0200

    Added MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+
+// Test for MongoDB
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
 
 var server = app.listen(3000, function () {
 
